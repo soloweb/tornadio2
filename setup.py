21dc6f3947953d1205a3c5cff5aba7f528edcb1f
Georgiy Kutsurua <g.kutsurua@ostrovok.ru>
2012-10-19 01:46:06 +0400

    change version

--- setup.py
+++ setup.py
@@ -18,13 +18,13 @@ except:
     readme = None
 
 setup(
-    name='TornadIO2',
-    version='0.0.3',
+    name='tornad_io2',
+    version='0.0.4',
     author='Serge S. Koval',
     author_email='serge.koval@gmail.com',
     packages=['tornadio2'],
     scripts=[],
-    url='http://github.com/MrJoes/tornadio2/',
+    url='https://github.com/soloweb/tornadio2/',
     license=license,
     description='Socket.io 0.7+ server implementation on top of Tornado framework',
     long_description=readme,
